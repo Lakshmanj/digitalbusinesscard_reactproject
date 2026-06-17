bfc138ea6c901242117844b4263bdbefeba819b5
Lakshmanj <32884137+Lakshmanj@users.noreply.github.com>
2026-06-17 17:46:14 -0400

    added react-icons dependancy for social icons

--- digitalbusinesscard/src/components/Info.jsx
+++ digitalbusinesscard/src/components/Info.jsx
@@ -1,15 +1,21 @@
 import profilePic from '../assets/pfp.jpg';
+import { FaLinkedin, FaPhoneAlt, FaEnvelope, FaGlobe, FaGithub } from 'react-icons/fa'
 
 
-export default function Info(){
-    return(
+export default function Info() {
+    return (
         <>
-        <div className='info'>
-            <img className='profile-pic' src={profilePic} alt="Profile Picture of Laksh Jairam" />
-            <h2>Laksh Jairam</h2>
-            <h3>Full-Stack Developer | Velos Digital Founder</h3>
-
-        </div>
+            <div className='info'>
+                <img className='profile-pic' src={profilePic} alt="Profile Picture of Laksh Jairam" />
+                <h2>Laksh Jairam</h2>
+                <h3>Full-Stack Developer | Velos Digital Founder</h3>
+                <div className='social-links'>
+                    <a href=""><FaEnvelope /></a>
+                    <a href=""><FaLinkedin /></a>
+                    <a href=""><FaGithub /></a>
+                    <a href=""><FaGlobe /></a>
+                </div>
+            </div>
         </>
     )
 }
