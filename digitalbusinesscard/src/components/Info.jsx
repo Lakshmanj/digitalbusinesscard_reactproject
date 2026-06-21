9ad4e8c0b618e66d3237e609a3ca9c7c7b4d3638
Lakshmanj <32884137+Lakshmanj@users.noreply.github.com>
2026-06-21 17:36:15 -0400

    updated social links with correct URLs and modified Velos Digital link in navbar

--- digitalbusinesscard/src/components/Info.jsx
+++ digitalbusinesscard/src/components/Info.jsx
@@ -10,10 +10,10 @@ export default function Info() {
                 <h2>Laksh Jairam</h2>
                 <h3>Full-Stack Developer | Velos Digital Founder</h3>
                 <div className='social-links'>
-                    <a href=""><FaEnvelope /></a>
-                    <a href=""><FaLinkedin /></a>
-                    <a href=""><FaGithub /></a>
-                    <a href=""><FaGlobe /></a>
+                    <a href="mailto:laksh.jairam@gmail.com" target="_blank" rel="noopener noreferrer"><FaEnvelope /></a>
+                    <a href="https://www.linkedin.com/in/lakshman-jairam/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+                    <a href="https://github.com/Lakshmanj" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+                    <a href="https://www.velosdigital.netlify.app" target="_blank" rel="noopener noreferrer"><FaGlobe /></a>
                 </div>
             </div>
         </>
--- digitalbusinesscard/src/components/Navbar.jsx
+++ digitalbusinesscard/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar(){
                 <ul className="resource-list">
                     <li><a href="https://github.com/Lakshmanj" target="_blank" rel="noopener noreferrer">GitHub</a></li>
                     <li><a href="https://www.linkedin.com/in/lakshman-jairam/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
-                    <li><a href="./">Velos Digital (Coming Soon!)</a></li>
+                    <li><a href="https://www.velosdigital.netlify.app">Velos Digital (Coming Soon!)</a></li>
                 </ul>
             </nav>
         </header>
