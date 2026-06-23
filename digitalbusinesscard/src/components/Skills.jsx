c9694c6776b29418f3dc9ff4150b794b42c9e418
Lakshmanj <32884137+Lakshmanj@users.noreply.github.com>
2026-06-23 16:47:47 -0400

    skills component

---
+++ digitalbusinesscard/src/components/Skills.jsx
@@ -0,0 +1,19 @@
+export default function Skills(){
+
+    return(
+        <>
+        <div className="skills-list">
+            <h3>Coding Languages & Frameworks/Libraries:</h3>
+            <ul>
+                <li>React</li>
+                <li>NextJS - React-Based Framework</li>
+                <li>Javascript</li>
+                <li>Django - Python Web Framework</li>
+                <li>Python</li>
+                <li>SpringBoot - Java Framework</li>
+                <li>Java</li>
+            </ul>
+        </div>
+        </>
+    )
+}
